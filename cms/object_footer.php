e6846a0d878783102397c8c8afb6e62166cdad79
creamgod45 <fuyin1054@gmail.com>
2020-06-23 11:30:17 +0800

    WTDB_PHP_ver.2.2.0

--- cms/object_footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="bdT ta-c p-30 lh-0 fsz-sm c-grey-600"><span>Copyright © 2020 無線二維條碼取餐APP All rights reserved. Designed by <a href="https://colorlib.com"
-            target="_blank" title="Colorlib">Colorlib</a>
-        </span>
-</footer>
-</div>
-</div>
-<script type="text/javascript" src="../assets/js/vendor.js"></script>
-<script type="text/javascript" src="../assets/js/bundle.js"></script>
